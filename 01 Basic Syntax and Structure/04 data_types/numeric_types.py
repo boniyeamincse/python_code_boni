1b62f5060f881430d2c7802ce906fad51836f2d4
boniyeamincse <boniyeamin.cse1@gmail.com>
2025-06-26 10:14:32 +0600

    Initial project upload

---
+++ 01 Basic Syntax and Structure/04 data_types/numeric_types.py
@@ -0,0 +1,33 @@
+# what is nemeric types in python
+# Numeric types in Python are used to represent numbers. There are three main numeric types:    
+
+#Numeric Types:	int, float, complex
+
+
+# integer type variable
+var1 = 10
+# float type variable
+var2 = 10.05
+# complex types variable 
+var3 = 3+4j
+
+
+print ( " integer type variable is: ", var1 )
+print ( " float type variable is: ", var2 )
+print ( " Complex types of variable " , var3)
+
+
+print ("\n--------------------- Types of Variable --------------------------------\n")
+
+print  (type(var1))
+print (type(var2))
+print (type(var3))
+print ("\n------------------------- ----------------------------------------------\n ")
+
+
+print ("\n--------------------- Addition --------------------------------\n")
+
+print ("Addition of integer and float is: ", var1 + var2)
+print ("Addition of integer and complex is: ", var1 + var3)
+print ("Addition of float and complex is: ", var2 + var3)
+
---
+++ 01 Basic Syntax and Structure/04 data_types/problem1.py
@@ -0,0 +1,9 @@
+# Data types 
+
+# text types data 
+
+var = input("Enter a string: ")
+print("The type of variable is:", type(var))
+
+# how much string is stored in the variable
+print("The length of the string is:", len(var))
